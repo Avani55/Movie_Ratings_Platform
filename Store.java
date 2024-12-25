f801c844074bcbd71d1f83f855fdbfce53b8e21b
Avani Srivasatava <113587404+Avani55@users.noreply.github.com>
2024-12-25 14:50:43 +0530

    Create Store.java

---
+++ Store.java
@@ -0,0 +1,48 @@
+class Store {
+
+  ArrayList<Movies> movies; // we have set the fields private, so that nobody can not access the objects field directly.
+
+  // constructor with no parameters, here constructor is being used 
+
+  public Store() {
+    movies = new ArrayList<>(); // shows the new refernce to the object.
+  }
+
+  // retrieve the movis based on the index of the list.
+  public Movies getMovies(int index) {
+    if (index >= 0 && index <= movies.size()) {
+      return movies.get(index);
+    }
+    return null;
+  }
+
+  // update a movie in the particular index.
+  public void setMovies(int index, Movies movie) {
+    if (index >= 0 && index <= movies.size()) {
+      movies.set(index, movie);
+    }
+  }
+
+  // add a new movie to the list.
+  public void addMovie(Movies movie) {
+    movies.add(movie);
+  }
+
+  // a function that prints all the movies in the store
+
+  public void printMovies() {
+    System.out.println(
+      "******************************MOVIE STORE*****************************"
+    );
+
+    for (int i = 0; i < movies.size(); i++) {
+      Movies movie = movies.get(i);
+      System.out.printf(
+        "%.1f\t%-12s\t%s\n",
+        movie.getRatings(),
+        movie.getFormat(),
+        movie.getTitle()
+      );
+    }
+  }
+}
